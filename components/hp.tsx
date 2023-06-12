3c62eeae3295c8c7aa11f906e9a7947304b83b80
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-12 12:27:17 +0300

    Convolutional Neural Network attempt.
    New stunt starts.
    
    Keep going.
    
    Remember why you started.

---
+++ components/hp.tsx
@@ -0,0 +1,14 @@
+import hp from "../styles/Hp.module.css";
+
+
+const Hp = () => {
+    return ( 
+        <>
+            <div className={hp.frame}>
+                <h1>Hello CNN</h1>
+            </div>
+        </>
+     );
+}
+ 
+export default Hp;

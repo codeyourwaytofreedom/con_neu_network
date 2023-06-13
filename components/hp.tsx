af8a5a4dbfa04cae23882e0a086dfd3f2721946f
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-13 20:35:04 +0300

    Feature extraction function added.
    Will use features to detect objects.
    
    Keep going.
    
    Remember why you started.

--- components/hp.tsx
+++ components/hp.tsx
@@ -13,11 +13,13 @@ const Hp = () => {
     const STOP_DATA_GATHER = -1;
 
     const [mobilnet, setMobileNet] = useState<GraphModel>();
-    let gatherDataState = STOP_DATA_GATHER;
-    let videoPlaying = false;
-    let trainingDataInputs = [];
-    let trainingDataOutputs = [];
-    let example_count = [];
+    const [gatherDataState,setGatherDataState] = useState(STOP_DATA_GATHER);
+    const [videoPlaying,setVidPlaying] = useState(false);
+    const [trainingDataInputs,setTrainingDataInputs] = useState<any>([]);
+    const [trainingDataOutputs,setTrainingDataOutputs] = useState<any>([]);
+
+    const [example_count,setExample_count] = useState<any>([]);
+
     let predict = false;
     let class_names = [];
 
@@ -34,6 +36,49 @@ const Hp = () => {
         })
     }
 
+    const hasGetUserMedia = () =>{
+        return !! (navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+    }
+
+    const enableCam = () => {
+        if(hasGetUserMedia()){
+            const constraints = {
+                video:true,
+                width:640,
+                height:480
+            }
+            navigator.mediaDevices.getUserMedia(constraints).then((stream)=>{
+                VIDEO.current!.srcObject = stream;
+
+            })
+        }
+    }
+
+    const gatherDataforClass = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const classNumber = parseInt(event.currentTarget.getAttribute('data-1hot')!);
+        setGatherDataState(gatherDataState === STOP_DATA_GATHER ? classNumber : STOP_DATA_GATHER)
+        dataGatherLoop();
+    };
+      
+    const dataGatherLoop = () => {
+        if(videoPlaying && gatherDataState !== STOP_DATA_GATHER){
+            let image_features = tf.tidy(()=>{
+                let videoFrameasTensor = tf.browser.fromPixels(VIDEO.current!);
+                let resized_Tensor_Frame = tf.image.resizeBilinear(videoFrameasTensor,[MN_INPUT_HEI,MN_INPUT_WID],true);
+                let normalizedTensorFrame = resized_Tensor_Frame.div(255);
+                return mobilnet!.predict(normalizedTensorFrame.expandDims().squeeze()); //could be an issue here. double check
+            });
+
+            setTrainingDataInputs([...trainingDataInputs,image_features]);
+            setTrainingDataOutputs([...trainingDataOutputs, gatherDataState]);
+
+/*             if(example_count[gatherDataState] === undefined){
+                example_count[gatherDataState] = 0;
+            } */
+
+            window.requestAnimationFrame(dataGatherLoop)
+        }
+    }
     useEffect(()=>{
         let model = tf.sequential();
         model.add(tf.layers.dense({inputShape:[1024],units:128,activation:"relu"}));
@@ -44,7 +89,6 @@ const Hp = () => {
             optimizer:"adam",
             loss:(class_names.length === 2) ? "binaryCrossentropy" : "categoricalCrossentropy",
             metrics:["accuracy"]
-
         });
     },[])
 
@@ -52,9 +96,11 @@ const Hp = () => {
         <>
             <div className={hp.frame}>
                 <h1>Hello CNN</h1>
-                <video autoPlay ref={VIDEO}/>
-                <button data-1hot = {0} ref={ENABLE_CAM_BUTTON}>Open Cam</button>
-                <button data-1hot = {1} ref={TRAIN_BUTTON}>Open Cam</button>
+                <video autoPlay ref={VIDEO} onLoadedData={()=> setVidPlaying(true)}/>
+                <button data-1hot = {0} ref={ENABLE_CAM_BUTTON} onClick={enableCam}>Open Cam</button>
+                <button data-1hot = {0} onClick={gatherDataforClass}>GAther1</button>
+                <button data-1hot = {1} onClick={gatherDataforClass}>Gather2</button>
+                <button ref={TRAIN_BUTTON}>XXX</button>
                 <button>Train & Predict</button>
             </div>
         </>
